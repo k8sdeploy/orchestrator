3da5ace47d961982f3297906be54ee1b36345a4a
Keloran <keloran@develbox.info>
2022-09-01 12:56:12 +0100

    make image name length 7

--- internal/orchestrator/grpc.go
+++ internal/orchestrator/grpc.go
@@ -46,6 +46,9 @@ func (s *Server) Deploy(ctx context.Context, in *pb.DeploymentRequest) (*pb.Depl
 	}
 	if in.K8SDetails.ImageHash != "" {
 		imageVersion = in.K8SDetails.ImageHash
+		if len(imageVersion) >= 7 {
+			imageVersion = fmt.Sprint("sha-%s", imageVersion[:7])
+		}
 	}
 	imageURL := fmt.Sprintf("%s:%s", "containers.chewedfeed.com/k8sdeploy/hooks-service", imageVersion)
 
