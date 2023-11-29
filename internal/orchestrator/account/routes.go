3097ecc6d38019dd847abb4f18619c7cd4b54a24
Keloran <keloran@develbox.info>
2023-11-29 10:59:09 +0000

    slight update

--- internal/orchestrator/account/routes.go
+++ internal/orchestrator/account/routes.go
@@ -1,7 +1,7 @@
 package account
 
 import (
-	"github.com/go-chi/chi/v5"
+	chi "github.com/go-chi/chi/v5"
 	"github.com/k8sdeploy/orchestrator-service/internal/config"
 	"net/http"
 )
@@ -19,5 +19,26 @@ func NewAccount(cfg *config.Config) *Account {
 func (a *Account) Router() http.Handler {
 	r := chi.NewRouter()
 
+	r.Get("/", a.HandleGet)
+	r.Put("/", a.HandlePut)
+	r.Post("/", a.HandlePost)
+	r.Delete("/", a.HandleDelete)
+
 	return r
+}
+
+func (a *Account) HandleDelete(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *Account) HandleGet(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *Account) HandlePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func (a *Account) HandlePut(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
 }
